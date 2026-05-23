b9eae60df4bd60fe63b6acb2db4af4abb8246ce3
Barath kumar <barathkumar5996@gmail.com>
2026-05-23 21:48:34 +0530

    changed database to sqlite

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -89,12 +89,8 @@ WSGI_APPLICATION = 'ecommerce_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ecommerce_db',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
